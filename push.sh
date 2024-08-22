830f0d23fc67604318f3aeec687865fae2a11626
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-22 12:18:14 +0000

    Analyse données et Atelier Ideation done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Problem Statement done"
+git commit -m "Analyse données et Atelier Ideation done"
 git push
