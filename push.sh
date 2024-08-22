fbf28cb16365b58bc8d5cd2c8cb310e7415e97d2
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-22 11:54:10 +0000

    Rename Diagram

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Analyse Données done"
+git commit -m "Rename Diagram"
 git push
