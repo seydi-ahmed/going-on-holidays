99c959325dd7fc1e06d8458767878fb38dfd620b
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-22 09:10:56 +0000

    etape 1 started

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "mini-framwork created"
+git commit -m "etape 1 started"
 git push
