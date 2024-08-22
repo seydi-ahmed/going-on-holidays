6b7c74029069150eefef5c4924c520f7fb23c6dc
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-22 11:59:32 +0000

    Problem Statement done

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Rename Diagram"
+git commit -m "Problem Statement done"
 git push
