40036527d8e2c25f9a4a7653498c272cc3a9f134
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2024-08-22 13:10:26 +0000

    Affinity diagram & Empathy map

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "Analyse données et Atelier Ideation done"
+git commit -m "Affinity diagram & Empathy map"
 git push
